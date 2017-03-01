40b07c23ae2d0d2c18cc426277bb2d94c0c3572d
Mutethia Gatobu <Evansgaji@gmail.com>
2017-02-28 17:18:23 -0700

    Add files via upload

--- practice.js
+++ practice.js
@@ -1,44 +1,74 @@
-//We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
   // 1) What is the purpose of the 'this keyword'?
-
+/*to call on the context of a particular function. In other words the function is the power source and the
+.this key opens a pathway to use that source on a seperate return.
       //Answer
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
+Implicit
+Explicit
 
       //Answer
 
   // 3) What is the difference between call and apply?
-
+.apply = array of paramaters (alot of data use this)
+.call = individual parameters (less data u use this one.)
       //Answer
 
   // 4) What does .bind do?
-
+.bind permanetly binds the certain parameters to the function in which you can call on later.
       //Answer
 
 
-//Next Problem
+//Next Problem*/
 
 //Create an object called user which has the following properties.
-  //username --> which is a string
-  //email --> which is a string
-  //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
+//username --> which is a string
+//email --> which is a string
+//getUsername --> which is a function that returns the current object's username property.
+//*Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
+  //Code Here
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
+var user = {
+    username: "tethi",
+    email: "evansgaji@gmail.com",
+    getUsername: function() {
+
+    return (this.username);
+}
+}
+
+user.getUsername()
+
+
 
 //Next Problem
 
 
-// Write a constructor function, including method definitions, which will make the following function invocations function properly.
+// Write a constructor function, including method definitions, which will make the following function
+//invocations function properly.
 
-  //Function Invocations Here
+function Car(brand, name, year) {
+  this.brand = brand;
+  this.name = name;
+  this.year = year;
+  this.move = 0;
+  this.moveCar = function() {
+    (this.move = this.move + 10)
+    return this.move;
+
+  }
+}
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
 
-//Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+//Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar
+//function which will increment the move property by 10. The move property will be added to every
+//object that is being returned from the Car function. You'll also need to use the 'this' keyword properly
+//in order to make sure you're invoking moveCar on the right object (prius vs mustang).
 
 prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
@@ -51,7 +81,8 @@ var getYear = function(){
   return this.year;
 };
 
-//Above you're given the getYear function. Call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a property on both objects*.
+//Above you're given the getYear function. Call the getYear function with the prius then the mustang objects being the focal objects.
+// *Don't add getYear as a property on both objects*.
 
 //Note(no tests)
   //Code Here
@@ -69,16 +100,15 @@ var getMyUsername = function() {
  return this.username;
 };
 
-var userName = getMyUsername(); //Fix this
+var userName = getMyUsername.call(myUser); //Fix this
 
 //Above you're given an object, and  a function. What will the getMyUsername function return?
 //Note(no tests)
-  //Answer Here
+  //the string 'iliketurtles'//Answer Here
 
 //In the example above, what is the 'this keyword' bound to when getMyUsername runs?
 
-  //Answer Here
+  //it is bound to the object MyUser--
 
 
 //Fix the getMyUsername invocation (stored in the userName variable, at the bottom of the above code) so that userName will be equal to 'iliketurtles'.
-
